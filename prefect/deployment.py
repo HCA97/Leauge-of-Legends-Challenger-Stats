db24c8cad97c1d9a8065e980d02753ff6e834dfc
HCA97 <h.cemaltun1997@gmail.com>
2023-03-31 15:48:04 +0200

    adding cron job

--- prefect/deployment.py
+++ prefect/deployment.py
@@ -1,6 +1,7 @@
 from prefect.deployments import Deployment
 from main_high_elo import process_data
 from prefect.filesystems import GCS
+from prefect.server.schemas.schedules import CronSchedule
 
 gcs_block = GCS.load("de-project-deployment")
 
@@ -12,7 +13,8 @@ gcs_deployment = Deployment.build_from_flow(
     parameters={
         'leauge': 'challengerleagues',
         'queue': 'RANKED_SOLO_5x5'
-    }
+    },
+    schedule=(CronSchedule(cron="0 0 * * *", timezone="UTC"))
 )
 
 if __name__ == "__main__":
